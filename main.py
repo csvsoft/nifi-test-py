2ea02c61e38ecfe6f9b7de79bf3cefb954e5433a
vinson <zhangruquan@hotmail.com>
2018-12-21 16:44:05 +0000

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("Hello, world!")
+import nipyapi
+def main():
+    nipyapi.config.nifi_config.host = "http://35.231.236.230:8080/nifi-api"
+    pg_id = nipyapi.canvas.get_root_pg_id()
+    print(pg_id)
+    processors = nipyapi.canvas.list_all_processors()
+    procCount = len(processors)
+    for p in processors:
+        print(p.id + " " + p.component.name + " " + p.status.run_status)
+        if p.status.run_status == "Stopped":
+            nipyapi.canvas.schedule_processor(p,True,True)
+    #print(procCount)
+
+if __name__ == "__main__":
+    main()    
